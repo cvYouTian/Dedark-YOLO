1504e345e7344ba1ae2ec0ce09fb1cb2b2ef60f9
cvYouTian <youclark00@gmail.com>
2025-05-23 11:42:00 +0800

    add v3 and darkdet

--- tt.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.utils import plot_model
-
-model = tf.keras.applications.EfficientNetB2()
-
-# 绘制网络结构
-plot_model(model, to_file='efficientnet_b2.png', show_shapes=True)
